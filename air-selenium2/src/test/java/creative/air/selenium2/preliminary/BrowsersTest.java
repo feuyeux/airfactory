ca2e2669a6276798a68f76a810e99fc643c2b54b
feuyeux <feuyeux@gmail.com>
2012-10-24 17:17:16 +0800

    update code for selenium test on ubuntu

---
+++ air-selenium2/src/test/java/creative/air/selenium2/preliminary/BrowsersTest.java
@@ -0,0 +1,48 @@
+package creative.air.selenium2.preliminary;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class BrowsersTest {
+	@Test
+	public void testGoogleSearch() {
+		WebDriver driver = null;
+		try {
+			driver = ChromeFactory.createDriver();
+			testSearch(driver);
+
+			driver = FirefoxFactory.createDriver();
+			testSearch(driver);
+
+			Properties props = System.getProperties();
+			String osName = props.getProperty("os.name");
+			if (osName.contains("Linux")) {
+			} else {
+				driver = IEFactory.createDriver();
+				testSearch(driver);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (driver != null)
+				driver.quit();
+		}
+	}
+
+	private void testSearch(WebDriver driver) {
+		driver.get("http://www.baidu.com");
+		WebElement searchBox = driver.findElement(By.id("kw"));
+		searchBox.sendKeys("JSF2和RichFaces4使用指南");
+		searchBox.submit();
+		assertEquals("百度搜索_JSF2和RichFaces4使用指南", driver.getTitle());
+	}
+}
--- air-selenium2/src/test/java/creative/air/selenium2/preliminary/SeleniumConfig.java
+++ air-selenium2/src/test/java/creative/air/selenium2/preliminary/SeleniumConfig.java
@@ -4,30 +4,32 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class SeleniumTest {
-	Properties properties = new Properties();
+public class SeleniumConfig {
+	static Properties properties = new Properties();
 
-	public SeleniumTest() {
+	static {
 		try {
 			properties.load(new FileInputStream("src/test/resources/selenium_config.properties"));
 		} catch (IOException e) {
 		}
 	}
 
-	public String getFirefoxPath() {
+	public static String getFirefoxPath() {
 		return properties.getProperty("seleniumTest.firefox.dir");
 	}
 
-	public String getChromePath() {
+	public static String getChromePath() {
 		return properties.getProperty("seleniumTest.chrome.dir");
 	}
 
-	public String getIEPath() {
+	public static String getIEPath() {
 		return properties.getProperty("seleniumTest.ie.dir");
 	}
 
 	public static void main(String[] args) {
-		SeleniumTest test = new SeleniumTest();
-		System.out.println(test.getFirefoxPath());
+		Properties props = System.getProperties();
+		String osName = props.getProperty("os.name");
+		System.out.println(osName);
+		System.out.println(getFirefoxPath());
 	}
 }
