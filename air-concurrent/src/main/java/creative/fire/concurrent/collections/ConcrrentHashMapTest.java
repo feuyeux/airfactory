684c2f9d8b1df5615e6aff0cb6183414da1508fe
Eric Han <feuyeux@gmail.com>
2013-05-31 22:07:53 +0800

    concurrent

---
+++ air-concurrent/src/main/java/creative/fire/concurrent/collections/ConcrrentHashMapTest.java
@@ -0,0 +1,11 @@
+package creative.fire.concurrent.collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcrrentHashMapTest {
+	private static ConcurrentHashMap<String, String> map=new ConcurrentHashMap<String, String>();
+	
+	public static void main(String[] args) {
+		
+	}
+}
